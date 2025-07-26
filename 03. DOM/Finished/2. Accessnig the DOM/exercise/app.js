7f1a0793aed80d7c5ce91c3ae662dd0e18eefcd8
theadityaa <80851233+theadityaa@users.noreply.github.com>
2025-07-27 00:15:33 +0530

    Update app.js

--- 03. DOM/Finished/2. Accessnig the DOM/exercise/app.js
+++ 03. DOM/Finished/2. Accessnig the DOM/exercise/app.js
@@ -1,6 +1,6 @@
 // 1. Select element h4 which holds text of "Red" by using getElementsByTagName
 // 2. Select div with the class of "green" which holds text (Green) by using getElementsByTagName
-// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByClassName
+// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsById
 // 4. Select div which has the class & Id of "yello" by using querySelector()
 // 5. Select all the elements which has the class of "teal" by using querySelectorAll
 
@@ -15,3 +15,8 @@ console.log(green);
 console.log(blue);
 console.log(yellow);
 console.log(allTeals);
+
+//In JavaScript DOM manipulation, the dot (.) is used as a 
+// selector for classes, while the hash symbol (#) is used 
+// as a selector for IDs. This applies when using methods 
+// like document.querySelector() or document.querySelectorAll().
